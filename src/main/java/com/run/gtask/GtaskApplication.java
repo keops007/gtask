b9c9d407153aba31e8c0105c0ae1513895749905
Catalin Holban <catalin@catalins-mbp.ro.kyndryl.net>
2024-08-01 17:10:42 +0300

    Add env dev si prod - and test with new table

--- src/main/java/com/run/gtask/GtaskApplication.java
+++ src/main/java/com/run/gtask/GtaskApplication.java
@@ -1,13 +1,23 @@
 package com.run.gtask;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GtaskApplication {
 
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GtaskApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		System.out.println("Active Profile: " + activeProfile);
+	}
 }
+
